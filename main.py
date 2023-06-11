13c0c9f47dac67c70f7cbdad777932fba2fbde08
Sumukha S <66332035+SumukhaS291299@users.noreply.github.com>
2023-06-11 16:46:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,53 @@
-import os
-
-import cv2
 import numpy as np
+import cv2
+import glob
+import imutils
+
+image_paths = glob.glob('Converted//images//*.jpg')
+images = []
 
 
-class Image_Stitching():
-    def __init__(self):
-        self.ratio = 0.85
-        self.min_match = 10
-        self.sift = cv2.xfeatures2d.SIFT_create()
-        self.smoothing_window_size = 800
-
-    def registration(self, img1, img2):
-        kp1, des1 = self.sift.detectAndCompute(img1, None)
-        kp2, des2 = self.sift.detectAndCompute(img2, None)
-        matcher = cv2.BFMatcher()
-        raw_matches = matcher.knnMatch(des1, des2, k=2)
-        good_points = []
-        good_matches = []
-        for m1, m2 in raw_matches:
-            if m1.distance < self.ratio * m2.distance:
-                good_points.append((m1.trainIdx, m1.queryIdx))
-                good_matches.append([m1])
-        img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good_matches, None, flags=2)
-        cv2.imwrite('matching.jpg', img3)
-        if len(good_points) > self.min_match:
-            image1_kp = np.float32(
-                [kp1[i].pt for (_, i) in good_points])
-            image2_kp = np.float32(
-                [kp2[i].pt for (i, _) in good_points])
-            H, status = cv2.findHomography(image2_kp, image1_kp, cv2.RANSAC, 5.0)
-        return H
-
-    def create_mask(self, img1, img2, version):
-        height_img1 = img1.shape[0]
-        width_img1 = img1.shape[1]
-        width_img2 = img2.shape[1]
-        height_panorama = height_img1
-        width_panorama = width_img1 + width_img2
-        offset = int(self.smoothing_window_size / 2)
-        barrier = img1.shape[1] - int(self.smoothing_window_size / 2)
-        mask = np.zeros((height_panorama, width_panorama))
-        if version == 'left_image':
-            mask[:, barrier - offset:barrier + offset] = np.tile(np.linspace(1, 0, 2 * offset).T, (height_panorama, 1))
-            mask[:, :barrier - offset] = 1
-        else:
-            mask[:, barrier - offset:barrier + offset] = np.tile(np.linspace(0, 1, 2 * offset).T, (height_panorama, 1))
-            mask[:, barrier + offset:] = 1
-        return cv2.merge([mask, mask, mask])
-
-    def blending(self, img1, img2):
-        H = self.registration(img1, img2)
-        height_img1 = img1.shape[0]
-        width_img1 = img1.shape[1]
-        width_img2 = img2.shape[1]
-        height_panorama = height_img1
-        width_panorama = width_img1 + width_img2
-
-        panorama1 = np.zeros((height_panorama, width_panorama, 3))
-        mask1 = self.create_mask(img1, img2, version='left_image')
-        panorama1[0:img1.shape[0], 0:img1.shape[1], :] = img1
-        panorama1 *= mask1
-        mask2 = self.create_mask(img1, img2, version='right_image')
-        panorama2 = cv2.warpPerspective(img2, H, (width_panorama, height_panorama)) * mask2
-        result = panorama1 + panorama2
-
-        rows, cols = np.where(result[:, :, 0] != 0)
-        min_row, max_row = min(rows), max(rows) + 1
-        min_col, max_col = min(cols), max(cols) + 1
-        final_result = result[min_row:max_row, min_col:max_col, :]
-        return final_result
+for image in image_paths:
+    img = cv2.imread(image)
+    images.append(img)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
 
 
-def main():
-    convertedpath = "Converted//images//"
-    IMAGES = os.listdir(convertedpath)
-    for i, img in enumerate(IMAGES):
-        baseimg = ""
-        if i == 0:
-            baseimg = img
-            continue
-        else:
-            final = Image_Stitching().blending(baseimg, img)
-            baseimg = final
-    cv2.imwrite('panorama.jpg', final)
+imageStitcher = cv2.Stitcher_create()
 
+error, stitched_img = imageStitcher.stitch(images)
 
-if __name__ == '__main__':
-    main()
+if not error:
+    cv2.imwrite("stitchedOutput.png", stitched_img)
+    cv2.imshow("Stitched Img", stitched_img)
+    cv2.waitKey(0)
+    stitched_img = cv2.copyMakeBorder(stitched_img, 10, 10, 10, 10, cv2.BORDER_CONSTANT, (0,0,0))
+    gray = cv2.cvtColor(stitched_img, cv2.COLOR_BGR2GRAY)
+    thresh_img = cv2.threshold(gray, 0, 255 , cv2.THRESH_BINARY)[1]
+    cv2.imshow("Threshold Image", thresh_img)
+    cv2.waitKey(0)
+    contours = cv2.findContours(thresh_img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    areaOI = max(contours, key=cv2.contourArea)
+    mask = np.zeros(thresh_img.shape, dtype="uint8")
+    x, y, w, h = cv2.boundingRect(areaOI)
+    cv2.rectangle(mask, (x,y), (x + w, y + h), 255, -1)
+    minRectangle = mask.copy()
+    sub = mask.copy()
+    while cv2.countNonZero(sub) > 0:
+        minRectangle = cv2.erode(minRectangle, None)
+        sub = cv2.subtract(minRectangle, thresh_img)
+    contours = cv2.findContours(minRectangle.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    areaOI = max(contours, key=cv2.contourArea)
+    cv2.imshow("minRectangle Image", minRectangle)
+    cv2.waitKey(0)
+    x, y, w, h = cv2.boundingRect(areaOI)
+    stitched_img = stitched_img[y:y + h, x:x + w]
+    cv2.imwrite("stitchedOutputProcessed.png", stitched_img)
+    cv2.imshow("Stitched Image Processed", stitched_img)
+    cv2.waitKey(0)
+else:
+    print("Images could not be stitched!")
+    print("Likely not enough keypoints being detected!")
